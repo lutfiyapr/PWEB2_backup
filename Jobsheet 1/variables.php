1a6307d1a48ca6a08683c4332940a7f52a95ef32
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2023-08-26 13:47:49 +0700

    Create variables.php

---
+++ Jobsheet 1/variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+    //Pendeklarasian Variabel
+    $intg=25; //contoh variabel untuk integer
+    $str="Praktikum Web 2"; // contoh variabel untuk string
+    $bl=true; //contoh variabel untuk boolean
+
+    //output
+    echo "Ini contoh penerapan Integer $intg </br>"; //menampilkan output variabel dengan value 25 dan memberikan enter
+    echo "Ini contoh penerapan String $str </br>"; //menampilkan output variabel dengan value Praktikum Web 2 dan memberikan enter
+    echo "Ini contoh penerapan Boolean $bl </br>";//menampilkan output variabel dengan value True dan memberikan enter
+    ?>
+</body>
+</html>
