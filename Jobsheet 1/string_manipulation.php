8f6b99cb34c310633c08bac117fdc47fd149a10b
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2023-08-28 09:37:20 +0700

    Create string_manipulation.php

---
+++ Jobsheet 1/string_manipulation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Manipulation</title>
+</head>
+<body>
+    <?php
+        //penjabaran variabel
+        $text = "Selamat Belajar PHP!";
+
+        //menampilkan variabel
+        echo "$text</br>";
+        //menampilkan variabel UPPERCASE
+        echo strtoupper ("$text</br>");
+        //menampilkan variabel lowercase
+        echo strtolower ("$text</br>");
+        //memotong isi variabel(string) pada bagian tertentu
+        echo substr($text, 8, 11);
+        //kenapa finish di 11?
+        //karena setelah menaruh 8 sebagai start, maka huruf (B)elajar bukan lagi huruf ke 8, tetapi menjadi huruf ke 0/reset
+    ?>
+</body>
+</html>
