793507fb3eae23fa1e4677d43f9cfab735e59036
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2023-08-30 14:30:55 +0700

    Create sidebar.php

---
+++ Jobsheet 3/asset/sidebar.php
@@ -0,0 +1,21 @@
+<nav id="sidebar" class="active">
+    <h><a href="#" class="logo">PWEB</a></h4>
+    <ul class="list-unstyled components mb-5">
+        <li>
+            <a href="tampil_mhs.php"><span class="bi bi-person-fill"></span>Mahasiswa</a>
+        </li>
+        <li class="active">
+            <a href="tampil_dosen.php"><span class="bi bi-person-fill"></span>Dosen</a>
+        </li>
+    </ul>
+    <div class="footer">
+        <p>
+        Project PWEB <script>document.write(new Date().getFullYear());</script><i class="icon-heart" aria-hidden="true"></i> Teknik Informatika 2C | by Lutfiya Ainurrahman.
+        </p>
+    </div>
+</nav>
+
+
+
+
+
